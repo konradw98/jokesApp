346c8aafc248a3f227522e46ccdd084b5cb285a8
konrad <konradwodnicki98@gmail.com>
2021-02-17 19:45:46 +0100

    creating joke controller

--- src/main/java/konrad/spring/jokesApp/services/JokeServiceImpl.java
+++ src/main/java/konrad/spring/jokesApp/services/JokeServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
-    
+
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
     public JokeServiceImpl(){
